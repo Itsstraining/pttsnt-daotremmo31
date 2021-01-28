9c8fece3b6c31ee2d1a4b1c28a2f8388edae7641
Kien <kien.ht1608@sinhvien.hoasen.edu.vn>
2021-01-28 23:32:28 +0700

    commit lan 2

--- program.js
+++ program.js
@@ -1,30 +1,30 @@
 // Add new functions, variables here
-function songuyento(input, i, arr="") {
+function songuyento(input, i, arr = " ") {
   while (input != 1) {
     if (input % i == 0) {
-      arr = arr+i + ""
+      arr = arr + i + " "
       input = input / i
     }
     else {
       i++
     }
-  }
+   }return arr;
 }
 
 
 
 function main(input) {
   let i = 2;
-  let arr = "";
-  if ( input == 1){
+  let arr = " ";
+  if (input == 1) {
     return 1;
   }
-  else if ( input == 0){
+  else if (input == 0) {
     return 0;
   }
-  else{
-  songuyento(input,i,arr);
-  console.log (arr);
+  else {
+    songuyento(input, i, arr);
+    console.log(songuyento(input,i,arr));
+  }
 }
-}
-  module.exports = main;
+module.exports = main;

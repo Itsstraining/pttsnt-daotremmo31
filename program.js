c1c1e843df603b71ea233e5676f1c4b3ad0cc084
Kien <kien.ht1608@sinhvien.hoasen.edu.vn>
2021-01-28 23:14:36 +0700

    commit lan 1

--- program.js
+++ program.js
@@ -1,8 +1,30 @@
 // Add new functions, variables here
-
-function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
+function songuyento(input, i, arr="") {
+  while (input != 1) {
+    if (input % i == 0) {
+      arr = arr+i + ""
+      input = input / i
+    }
+    else {
+      i++
+    }
+  }
 }
 
-module.exports = main;
+
+
+function main(input) {
+  let i = 2;
+  let arr = "";
+  if ( input == 1){
+    return 1;
+  }
+  else if ( input == 0){
+    return 0;
+  }
+  else{
+  songuyento(input,i,arr);
+  console.log (arr);
+}
+}
+  module.exports = main;
